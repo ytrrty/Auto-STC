cb96ffc3335bfc1483e3058096723150d1dec49a
Alexander Korol <korol.sas@mail.ru>
2015-02-12 15:12:45 +0200

    fix width features list and add view type features

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -2,7 +2,7 @@ class Feature < ActiveRecord::Base
   has_many :features_configuration
   has_many :configurations, through: :features_configuration
 
-  enum type: [:security, :technology]
+  enum type: [:security, :technology, :view]
 
   validates_presence_of :name
   validates :typec, presence: true, inclusion: { in: Feature.types.keys, message: "%{value} is not a valid type" }

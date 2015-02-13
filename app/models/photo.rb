a52091bad62db194eee87b11ea8b0d9c4f43da8c
Alexander Korol <korol.sas@mail.ru>
2015-02-13 11:11:27 +0200

    Page info for cars is workgit add .!

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,8 @@
 class Photo < ActiveRecord::Base
   belongs_to :car
 
+  has_attached_file :file, default_url: "/images/no_car_img.jpg"
+
   validates_presence_of :name, :car_id
   validates_attachment_content_type :file, content_type: /\Aimage\/.*\Z/
 end
